f0730b854d3d9604186cc8f0fee9e1cb3774b89f
Ted Kulp <ted@tedkulp.com>
2018-11-14 07:53:11 -0500

    API cleanup. Add more channel status to state.
    - API is redone to centralize calls to twitch API
    - Reworked capture of limit headers to account for different types
      of calls
    - Added rest of data to state.  Game title, num viewers, etc.
      - Updates every 30 seconds
    - Added events for state change and game change
      - So we can update the panel
      - So that we can add Grafana tracking later
      - Still need to add a few more for offline, online

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,68 +1,141 @@
 import moment from 'moment';
 import { setup } from 'axios-cache-adapter';
-import { get } from 'lodash';
+import { get, assign } from 'lodash';
+import { inspect } from 'util';
 
-import log from './logger';
 import state from './state';
 import config from './config';
 import { UserModel } from './models/user';
 import logger from './logger';
 import redis from './servers/redis';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
-const axios = setup({
+const axiosCache = setup({
     cache: {
         maxAge: 5 * 1000,
         exclude: { query: false },
         key: req => req.url + JSON.stringify(req.params),
     },
-});
+}) as AxiosInstance;
 
-const getStreamData = async () => {
-    try {
-        const response = await axios.get('https://api.twitch.tv/helix/streams', {
-            headers: {
-                'Client-ID': config.getClientId(),
-            },
-            params: {
-                user_login: config.getStreamerName(),
-            },
-        });
+const axiosLongCache = setup({
+    cache: {
+        exclude: { query: false },
+        key: req => req.url + JSON.stringify(req.params),
+    },
+}) as AxiosInstance;
 
-        log.debug(['response from cache', response.request.fromCache ? 'true' : 'false']);
+export enum RequestAuthType {
+    NONE,
+    CLIENT,
+    BOT_OAUTH,
+    STREAMER_OAUTH,
+};
 
-        state.setApiLimit(response.headers['ratelimit-limit'], response.headers['ratelimit-remaining'], response.headers['ratelimit-reset']);
+export enum RequestCacheType {
+    NONE,
+    SHORT,
+    LONG,
+};
 
-        return get(response, 'data.data[0]', []);
-    } catch (e) {
-        log.error(e); // TODO: Finalize API handling
+export const makeHelixRequest = async (requestConfig: AxiosRequestConfig, authType = RequestAuthType.CLIENT, cacheType = RequestCacheType.SHORT) => {
+    requestConfig = assign({}, {
+        baseURL: 'https://api.twitch.tv/helix',
+        headers: {},
+    }, requestConfig);
+
+    switch (authType) {
+        case RequestAuthType.CLIENT:
+            requestConfig.headers['Client-ID'] = config.getClientId();
+            break;
+        case RequestAuthType.BOT_OAUTH:
+            requestConfig.headers['Client-ID'] = config.getClientId();
+            requestConfig.headers['Authorization'] = `Bearer ${await redis.get('bot:oauth:access_token')}`;
+            break;
+        case RequestAuthType.STREAMER_OAUTH:
+            requestConfig.headers['Client-ID'] = config.getClientId();
+            requestConfig.headers['Authorization'] = `Bearer ${await redis.get('streamer:oauth:access_token')}`;
+            break;
+    };
+
+    let fn: Function = axios;
+    if (cacheType == RequestCacheType.SHORT) {
+        fn = axiosCache;
+    } else if (cacheType == RequestCacheType.LONG) {
+        fn = axiosLongCache;
     }
 
-    return [];
+    const response = await fn(requestConfig);
+
+    setApiLimits(authType, response);
+
+    return response;
+};
+
+export const makeKrakenRequest = async (requestConfig: AxiosRequestConfig, authType = RequestAuthType.CLIENT, allowCache = true) => {
+    requestConfig = assign({}, {
+        baseURL: 'https://api.twitch.tv/kraken',
+        headers: {},
+    }, requestConfig);
+
+    switch (authType) {
+        case RequestAuthType.CLIENT:
+            requestConfig.headers['Client-ID'] = config.getClientId();
+            break;
+        case RequestAuthType.BOT_OAUTH:
+            requestConfig.headers['Client-ID'] = config.getClientId();
+            requestConfig.headers['Authorization'] = `OAuth ${await redis.get('bot:oauth:access_token')}`;
+            break;
+        case RequestAuthType.STREAMER_OAUTH:
+            requestConfig.headers['Client-ID'] = config.getClientId();
+            requestConfig.headers['Authorization'] = `OAuth ${await redis.get('streamer:oauth:access_token')}`;
+            break;
+    };
+
+    const response = allowCache ? await axiosCache(requestConfig) : axios(requestConfig);
+    setApiLimits(authType, response);
+
+    if (allowCache) {
+        logger.debug(['response from cache', (response as any).request.fromCache ? 'true' : 'false']);
+    }
+
+    return response;
+};
+
+const setApiLimits = (authType, response) => {
+    const h = response.headers;
+    console.log('authType', authType);
+    switch (authType) {
+        case RequestAuthType.BOT_OAUTH:
+            state.setApiLimit('BOT', h['ratelimit-limit'], h['ratelimit-remaining'], h['ratelimit-reset']);
+            break;
+        case RequestAuthType.STREAMER_OAUTH:
+            state.setApiLimit('STREAMER', h['ratelimit-limit'], h['ratelimit-remaining'], h['ratelimit-reset']);
+            break;
+        case RequestAuthType.CLIENT:
+            state.setApiLimit('CLIENT', h['ratelimit-limit'], h['ratelimit-remaining'], h['ratelimit-reset']);
+            break;
+        default:
+            state.setApiLimit('NONE', h['ratelimit-limit'], h['ratelimit-remaining'], h['ratelimit-reset']);
+    }
 }
 
-const getOnlineStatus = async () : Promise<boolean> => {
-    const data = await getStreamData();
-    if (data && data.type === 'live') {
-        return true;
-    }
+export const getStreamData = async () => {
+    const response = await makeHelixRequest({
+        url: '/streams',
+        params: {
+            // user_login: config.getStreamerName(),
+            user_login: 'pixelogicdev',
+        },
+    });
 
-    return false;
-};
+    return get(response, 'data.data[0]', {});
+}
 
-const getOnlineStartTime = async () : Promise<moment.Moment | null> => {
-    const data = await getStreamData();
-    if (data && data.started_at) {
-        return moment(data.started_at);
-    }
-
-    return null;
-};
-
-const getUserDetails = async (username?: string) => {
+export const getUserDetails = async (username?: string) => {
     let foundUser = await UserModel.findOne({
         'username': username
     });
-    // logger.info(['foundUser', foundUser]);
 
     if (foundUser) {
         return foundUser;
@@ -85,50 +158,29 @@ const getUserDetails = async (username?: string) => {
     return null;
 };
 
-const getUserDetailsFromApi = async (username?: string) => {
-    try {
-        const response = await axios.get('https://api.twitch.tv/helix/users', {
-            headers: {
-                'Client-ID': config.getClientId(),
-            },
-            params: {
-                login: username || config.getStreamerName(),
-            },
-        });
+export const getUserDetailsFromApi = async (username?: string) => {
+    const response = await makeHelixRequest({
+        url: '/users',
+        params: {
+            login: username || config.getStreamerName(),
+        },
+    });
 
-        log.debug(['response from cache', response.request.fromCache ? 'true' : 'false']);
-
-        state.setApiLimit(response.headers['ratelimit-limit'], response.headers['ratelimit-remaining'], response.headers['ratelimit-reset']);
-
-        return get(response, 'data.data[0]', null);
-    } catch (e) {
-        log.error(e); // TODO: Finalize API handling
-    }
-
-    return null;
+    return get(response, 'data.data[0]', null);
 };
 
-const getUserDetailsFromToken = async (token: string) => {
-    try {
-        const response = await axios.get('https://api.twitch.tv/helix/users', {
-            headers: {
-                'Authorization': `Bearer ${token}`,
-            },
-        });
+export const getUserDetailsFromToken = async (token: string) => {
+    const response = await makeHelixRequest({
+        url: '/users',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+        }
+    }, RequestAuthType.NONE);
 
-        log.debug(['response from cache', response.request.fromCache ? 'true' : 'false']);
-
-        state.setApiLimit(response.headers['ratelimit-limit'], response.headers['ratelimit-remaining'], response.headers['ratelimit-reset']);
-
-        return get(response, 'data.data[0]', null);
-    } catch (e) {
-        log.error(e); // TODO: Finalize API handling
-    }
-
-    return null;
+    return get(response, 'data.data[0]', null);
 }
 
-const getUserId = async (username?: string) => {
+export const getUserId = async (username?: string) => {
     const details = await getUserDetails(username);
     if (details) {
         return details.twitchId;
@@ -138,42 +190,38 @@ const getUserId = async (username?: string) => {
 };
 
 const getStreamerFollowsData = async () => {
-    try {
-        const response = await axios.get('https://api.twitch.tv/helix/users/follows', {
-            headers: {
-                'Client-ID': config.getClientId(),
-            },
-            params: {
-                to_id: await redis.getAsync('streamer:twitch_id'),
-            },
-        });
+    const response = await makeHelixRequest({
+        url: '/users/follows',
+        params: {
+            to_id: await redis.getAsync('streamer:twitch_id'),
+        },
+    });
 
-        log.debug(['response from cache', response.request.fromCache ? 'true' : 'false']);
-
-        state.setApiLimit(response.headers['ratelimit-limit'], response.headers['ratelimit-remaining'], response.headers['ratelimit-reset']);
-
-        return get(response, 'data', null);
-    } catch (e) {
-        log.error(e); // TODO: Finalize API handling
-    }
-
-    return null;
+    return get(response, 'data', null);
 };
 
-// const getStreamerFollows = async () => {
-// };
-
-const getStreamerFollowCount = async () => {
+export const getStreamerFollowCount = async () => {
     const followData = await getStreamerFollowsData();
     return get(followData, 'total', 0);
 };
 
+export const getGameInfo = async (gameId) => {
+    const response = await makeHelixRequest({
+        url: '/games',
+        params: {
+            id: gameId,
+        },
+    }, RequestAuthType.CLIENT, RequestCacheType.LONG);
+
+    return get(response, 'data.data[0]', null);
+}
+
 export default {
+    RequestAuthType,
     getStreamData,
-    getOnlineStatus,
-    getOnlineStartTime,
     getUserDetails,
     getUserDetailsFromToken,
     getUserId,
     getStreamerFollowCount,
+    getGameInfo,
 };
--- backend/src/state.ts
+++ backend/src/state.ts
@@ -1,22 +1,85 @@
-import moment from 'moment';
+import moment, { now } from 'moment';
+import { isEmpty, has, get } from 'lodash';
 import api from './api';
 import log from './logger';
+import events from './events';
+import logger from './logger';
+
+type apiLimitValues = {
+    limit: number | null,
+    remaining: number | null,
+    refresh: moment.Moment | null,
+};
+
+type apiLimitMap = { [s: string]: apiLimitValues };
 
 class State {
-
     online: boolean = false;
     onlineStartTime: moment.Moment | null = null;
-    apiLimit = {
-        limit: null as number | null,
-        remaining: null as number | null,
-        refresh: null as moment.Moment | null,
-    };
+    apiLimits: apiLimitMap = {};
+    numViewers: number | null = 0;
+    title: string | null = '';
+    gameId: number | null = 0;
+    gameTitle: string | null = '';
+    language: string | null = '';
 
     async init() {
-        this.setOnline(await api.getOnlineStatus());
-        this.setOnlineStartTime(await api.getOnlineStartTime());
+        events.register('status', 'stream', 'Constantly updated status of stream. Fires every 30 seconds.');
+        events.register('status', 'changegame', 'Fired when the game changes');
+        this.updateStatus();
+        setInterval(this.updateStatus.bind(this), 30 * 1000);
+    }
 
-        log.info(['state init', this]);
+    async updateStatus() {
+        try {
+            const streamData = await api.getStreamData();
+            this.setNumViewers(parseInt(get(streamData, 'viewer_count', 0)));
+            this.setTitle(get(streamData, 'title', ''));
+            this.setLanguage(get(streamData, 'language', 'en'));
+            this.setOnline(get(streamData, 'type', '') === 'live');
+            this.setOnlineStartTime(has(streamData, 'started_at') ? moment(get(streamData, 'started_at')) : null);
+            await this.setGameId(parseInt(get(streamData, 'game_id', 0)));
+
+            events.trigger('status', 'stream', this.toJSON());
+
+            log.debug(['current state', this]);
+        } catch (e) {
+            // TODO: What are we doing with errors?
+            console.error(e);
+        }
+    }
+
+    setNumViewers(numViewers: number) {
+        this.numViewers = numViewers;
+    }
+
+    setTitle(title: string) {
+        this.title = title;
+    }
+
+    async setGameId(gameId: number) {
+        if (this.gameId !== gameId) {
+            logger.debug('Updating game title');
+
+            const gameTitle = get(await api.getGameInfo(gameId), 'name', '');
+            this.setGameTitle(gameTitle);
+
+            if (!isEmpty(gameTitle)) {
+                logger.debug('Trigger changegame event');
+                events.trigger('status', 'changegame', {
+                    title: gameTitle,
+                });
+            }
+        }
+        this.gameId = gameId;
+    }
+
+    setGameTitle(gameTitle: string) {
+        this.gameTitle = gameTitle;
+    }
+
+    setLanguage(language: string) {
+        this.language = language;
     }
 
     isOnline() {
@@ -44,14 +107,32 @@ class State {
         this.onlineStartTime = null;
     }
 
-    getApiLimit() {
-        return this.apiLimit;
+    getApiLimit(type) {
+        return this.apiLimits[type];
     }
 
-    setApiLimit(limit, remaining, refresh) {
-        this.apiLimit.limit = limit;
-        this.apiLimit.remaining = remaining;
-        this.apiLimit.refresh = moment.unix(refresh);
+    setApiLimit(type: string, limit, remaining, refresh) {
+        const limits: apiLimitValues = {
+            limit,
+            remaining,
+            refresh: moment.unix(refresh),
+        };
+
+        this.apiLimits[type] = limits;
+
+        console.log(this.apiLimits);
+    }
+
+    toJSON() {
+        return {
+            online: this.online,
+            onlineStartTime: this.onlineStartTime ? this.onlineStartTime.format() : '',
+            numViewers: this.numViewers,
+            title: this.title,
+            gameId: this.gameId,
+            gameTitle: this.gameTitle,
+            language: this.language,
+        };
     }
 }
 
